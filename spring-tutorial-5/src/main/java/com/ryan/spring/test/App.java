932524a404b8d0c1d07112c875d0035e0237ad7e
JiahuiRuan <jr58@rice.edu>
2017-08-24 11:05:26 -0500

    finish spring hello world and class path import

--- spring-tutorial-5/src/main/java/com/ryan/spring/test/App.java
+++ spring-tutorial-5/src/main/java/com/ryan/spring/test/App.java
@@ -1,11 +1,19 @@
 package com.ryan.spring.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person person = new Person();
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/ryan/spring/test/beans/beans.xml");
+		
+		Person person = (Person)context.getBean("person");
 		person.speak();
+		
+		((ClassPathXmlApplicationContext)context).close();
 	}
 
 }
